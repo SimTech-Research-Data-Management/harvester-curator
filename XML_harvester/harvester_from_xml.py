b0887e511b04bf129732ecc98334cad0d2251931
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2023-05-12 08:02:54 +0200

    Changed Repo Structure

---
+++ XML_harvester/harvester_from_xml.py
@@ -0,0 +1,43 @@
+#import xml.etree.ElementTree as ET
+#from pyDaRUS import Citation, Dataset
+
+# parse the XML file
+#tree = ET.parse('author_name.xml')
+#root = tree.getroot()
+
+# find and get the elements
+#for title in root.iter('title'):
+#    Title = title.get('title')
+#    year = title.find('year').text
+#    for author in title.iter('author_name'):
+#        name = author.get('name')
+
+from lxml import etree
+from pyDaRUS import Citation, Dataset
+
+# parse the XML file
+tree = etree.parse('author_name.xml')
+root = tree.getroot()
+
+# find and get the elements
+for title in root.xpath('//title'):
+    Title = title.get('title')
+    year = title.find('year').text
+    for author in title.xpath('./author_name'):
+        name = author.get('name')
+
+# Initialize Dataset
+dataset = Dataset()
+
+# Initialize metadatablocks you like to use
+citation = Citation()
+
+# Add author name to the citation metadata
+citation.add_author(name=name)
+citation.title = Title
+citation.add_description(date=year)
+
+# Add the citation metadatablock to the dataset
+dataset.add_metadatablock(citation)
+
+print(citation.author, citation.title, citation.description)
