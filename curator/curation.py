03634415c8b39a80fe25ddab085782ba9c4d08fb
Sarbani Roy <sarbani.roy@iws.uni-stuttgart.de>
2023-08-02 21:12:39 +0200

    Succesfully uploaded citation metadata to demodarus

--- curator/curation.py
+++ curator/curation.py
@@ -1,4 +1,7 @@
 import json
+from pyDaRUS import Dataset, Citation, Privacy
+from pyDaRUS.metadatablocks.citation import SubjectEnum
+
 
 def get_json_classes(json_data):
     classes = set()
@@ -12,13 +15,40 @@ def get_json_classes(json_data):
     extract_classes(json_data)
     return classes
 
+def generate_commands(json_data, schema):
+    commands = []
+
+    def extract_properties(class_name, class_data):
+        class_prop = schema.get('definitions', {}).get(class_name, {}).get('properties', {})
+        if class_prop:
+            command = f"citation.add_{class_name}("
+            for prop_name, prop_info in class_prop.items():
+                if prop_name in class_data:
+                    prop_value = class_data.get(prop_name)
+                    command = command + f'{prop_name} = "{prop_value}", '
+            command = command[:-2] + ")"
+            commands.append(command)
+        else:
+            command = f'citation.{class_name} = "{class_data}"'
+            commands.append(command)
+
+    example_json_classes = get_json_classes(json_data)
+
+    for class_name, class_info in classes_info.items():
+        if class_name in example_json_classes:
+            example_json_class_data = example_json_data[class_name]
+            if class_info['type_class'] == "compound":
+                for data in example_json_class_data:
+                    extract_properties(class_name, data)
+            else:
+                extract_properties(class_name, example_json_class_data)
+
+    return commands
+
 # Load the example JSON file
 example_json_file = "citation_meta_ex.json"
 with open(example_json_file) as file:
     example_json_data = json.load(file)
-
-# Get the classes from the example JSON file
-example_json_classes = get_json_classes(example_json_data)
 
 citation_metadata_file = "./metadata_schema_DaRUS/citation.json"
 # Load the JSON schema
@@ -35,42 +65,36 @@ for class_name, class_info in properties.items():
     class_type_class = class_info.get('typeClass')
     classes_info[class_name] = {'type': class_type, 'type_class': class_type_class}
 
+# Initialize the dataset
+dataset = Dataset()
 
-# the extracted information
-for class_name, class_info in classes_info.items():
-    if class_name in example_json_classes:
-        #get the data of a particular class from the json file of harvested metadata
-        example_json_class_data = example_json_data[class_name]
+# Initialize metadatablock
+citation = Citation()
+privacy = Privacy()
 
-        if class_info['type_class'] == "compound":
-            # Get the definitions from the schema
-            class_prop = schema.get('definitions', {}).get(class_name, {}).get('properties', {})
+# Fill in citation relevant fields
+citation.subject = [SubjectEnum.mathematical__sciences]
 
-            if class_prop:
-                for data in example_json_class_data:
-                    command = f"citation.add_{class_name}("
-                    for prop_name, prop_info in class_prop.items():
-                        if prop_name in data:
-                            prop_value = data.get(prop_name)
-                            command = command + f'{prop_name} = "{prop_value}", '
-                    command = command[:-2] + ")"
-                    print(command)        
-            else:
-                command = f'citation.add_{class_name} = "{example_json_class_data}"'
+# Generate commands
+commands = generate_commands(example_json_data, schema)
 
-        else:
-            #Todo: validate datatype before pushing
-            #print(type(example_json_class_data).__name__)
-            #print(class_info['type'])
-            class_prop = schema.get('definitions', {}).get(class_name, {}).get('properties', {})
-            if class_prop:
-                for data in example_json_class_data:
-                    command = f"citation.add_{class_name}("
-                    for prop_name, prop_info in class_prop.items():
-                        if prop_name in data:
-                            prop_value = data.get(prop_name)
-                            command = command + f'{prop_name} = "{prop_value}", '
-                    command = command[:-2] + ")"
-                    print(command)
-            else:
-                command = f'citation.{class_name} = "{example_json_class_data}"'
+# Execute the commands
+for command in commands:
+    try:
+        exec(command)
+        print(f"Executed: {command}")
+    except Exception as e:
+        print(f"Failed to execute: {command}")
+        print(f"Error: {e}")
+
+# Fill in privacy relevant fields
+privacy.personal_data = "no"
+
+# Add each metadatablock to the dataset
+dataset.add_metadatablock(citation)
+dataset.add_metadatablock(privacy)
+
+# Upload the dataset
+p_id = dataset.upload (dataverse_name="roy_dataverse")
+#dataset.update(contact_name="Sarbani Roy", contact_email="sarbani.roy@simtech.uni-stuttgart.de")
+print('Dataset created and directory uploaded successfully.')
