be210edc5087d3bb292f4e54e3cdfd197ad676cb
Sarbani-Roy <98636981+Sarbani-Roy@users.noreply.github.com>
2024-02-07 09:31:34 +0100

    [uploader] Successful upload using EasyDataverse

--- curator/dataset_upload.py
+++ curator/dataset_upload.py
@@ -1,18 +1,3 @@
-# from easyDataverse import Dataverse
-
-# dataverse = Dataverse(
-#   server_url="https://darus.uni-stuttgart.de",
-#   api_token="3be89818-d34c-422d-a3fb-136d5cd253dc",
-# )
-
-# json_file_path = "/Users/sarbani/Simtech_works/Harvester-Curator/example/md_com.json"
-
-# # Open the JSON file in read mode
-# with open(json_file_path, 'r') as json_file:
-#     dataset = dataverse.dataset_from_json(json_file)
-
-# print(dataset)
-
 import os
 import json
 from easyDataverse import Dataverse
@@ -33,7 +18,7 @@ else:
 # Initialize Dataverse instance
 dataverse = Dataverse(
     server_url="https://darus.uni-stuttgart.de",
-    api_token="3be89818-d34c-422d-a3fb-136d5cd253dc",
+    api_token="",
 )
 
 # Open the JSON file in read mode
